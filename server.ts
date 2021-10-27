1504e0e391e885352afb1f040d30195ccaa2a9d8
Andrei <andreiferreira@outlook.com>
2021-10-26 21:45:49 -0300

    proeject configured

--- server.ts
+++ server.ts
@@ -1,1 +1,13 @@
-console.log('aaaaaaaa');
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('funfando');
+});
+
+app.listen(1234, () => {
+  console.log('servidor rodando na porta 1234');
+});
