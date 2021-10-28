e1239a01276521d0799563e9461ef3e4a019622d
Andrei <andreiferreira@outlook.com>
2021-10-27 21:17:51 -0300

    feat(user/entity) user entity created

---
+++ src/modules/users/typeorm/entities/User.ts
@@ -0,0 +1,24 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('users')
+class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export { User };
